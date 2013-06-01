661c7b19accbeb0a45a8b527d1038340010a886a
Abel <abel.ramv@gmail.com>
2013-06-01 17:39:33 -0500

    Another test -_-

--- js/localizacion.js
+++ js/localizacion.js
@@ -1,15 +1,1 @@
 
-var pos;
-var onSuccess = function(position) {
-    pos=position;
-};
-
-
-
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('Hola');
-};
-
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
